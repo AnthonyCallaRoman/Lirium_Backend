d1bd02156639686aeaa2c217f42c19c92460aec1
Anthony Calla Roman <55769459+AnthonyCallaRoman@users.noreply.github.com>
2024-09-19 01:31:09 +0000

    agregar modelo producto

---
+++ models/producto.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const productoSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+    },
+    imagen: {
+        type: String,
+        required: true,
+    },
+    cantidad: {
+        type: Number,
+        required: true,
+    },
+    precio_compra: {
+        type: Number,
+        required: true
+    },
+    marca: {
+        type: String,
+        required: true,
+    },
+    descripcion: {
+        type: String,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('Producto', productoSchema);
