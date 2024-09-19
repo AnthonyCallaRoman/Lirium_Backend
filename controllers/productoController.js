0137e19b4c47dc8e07fd731100090af16340fa6a
AnthonyCallaRoman <anthonycallaroman97@gmail.com>
2024-09-19 16:39:02 -0500

    an me falta pero avance el productoController

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,12 +1,15 @@
 const productoService = require('../services/productoService');
 
 class ProductoController {
-    async createProducto(req, res) {
-        try{
-
-        }catch (error){
-            
-        }
-
+  async createProducto(req, res) {
+    try {
+      const producto = await productoService.createProducto(req.body);
+      res.status(201).json(producto);    
+    } catch (error) {
+      res.status(400).json({ message: error.message });
     }
+  }
+  async getProducto(rew, res) {
+    
+  }
 }
