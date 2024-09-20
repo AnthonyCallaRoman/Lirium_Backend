08e2590bb5c7ee814f620a47feabee159d8234e6
Anthony Calla Roman <55769459+AnthonyCallaRoman@users.noreply.github.com>
2024-09-20 02:36:25 +0000

    cuadrando esquema productos

--- models/producto.js
+++ models/producto.js
@@ -1,30 +1,21 @@
 const mongoose = require('mongoose');
 
 const productoSchema = new mongoose.Schema({
-    nombre: {
-        type: String,
-        required: true,
-    },
-    imagen: {
-        type: String,
-        required: true,
-    },
-    cantidad: {
-        type: Number,
-        required: true,
-    },
-    precio_compra: {
-        type: Number,
-        required: true
-    },
-    marca: {
-        type: String,
-        required: true,
-    },
-    descripcion: {
-        type: String,
-        required: true,
-    }
+    nombre: { type: String, required: true, },
+    descripcion: { type: String, required: true, },
+    categoria
+    cantidad_stock
+    unidad_medida
+    marca
+    fecha_cadusidad
+    ubicacion_almacen
+    proveedor
+    fecha_ingreso
+    imagen: { type: String, required: true, },
+    cantidad: { type: Number, required: true, },
+    precio_compra: { type: Number, required: true },
+    marca: { type: String, required: true, },
+    
 });
 
 module.exports = mongoose.model('Producto', productoSchema);
