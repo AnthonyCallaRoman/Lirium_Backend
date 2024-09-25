7209fed1a3f836fe481b658dec19e3cf4260ee11
Anthony Calla Roman <anthonycallaroman97@gmail.com>
2024-09-24 21:39:04 -0500

    instalacion de cors

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const express = require('express');
 const connectDB = require('./config/database');
 const dotenv = require('dotenv');
 const morgan = require('morgan')
+const cors = require('cors')
 dotenv.config();
 
 const app = express();
 
 app.use(morgan('combined'));
+app.use(cors());
 
 connectDB();
 
@@ -23,7 +25,7 @@ app.use('/categoria', CategoriaRoutes);
 app.use('/subCategoria', SubCategoriaRoutes);
 app.use('/unidadMedida', UnidadMedidaRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
